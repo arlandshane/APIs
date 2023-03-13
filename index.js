558c72349ff6a1d1b8dd85b33e5f17b2c0c542c0
Shane Arland <arlandshane@gmail.com>
2023-03-13 15:31:45 +0530

    using ejs and the movie db api

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const express = require("express");
 const https = require("https");
+const ejs = require("ejs");
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-	https.get("https://api.waifu.pics/nsfw/blowjob", function (response) {
+	const apiKey = "edf8b0fa6d19119eabe2d4ba381620ae";
+	var movieName = "gargi";
+	var year = "2022";
+	const api =
+		"https://api.themoviedb.org/3/search/movie?query=" +
+		movieName +
+		"&api_key=" +
+		apiKey +
+		"&year=" +
+		year;
+	https.get(api, function (response) {
 		response.on("data", function (data) {
-			answer = JSON.parse(data).url;
-			res.send(
-				"<h1>Hi</h1><img style='width: 30rem' src=" + answer + ">"
-			);
+			var answer = JSON.parse(data).results[0].overview;
+			res.render("home", { overview: answer });
+			console.log(answer);
 		});
 	});
 });
 
+// app.post("/", (req, res) => {
+// 	res.sendFile(__dirname + "/index.ejs");
+// });
+
 app.listen(port, () => {
-	console.log("Server started successfully");
+	console.log("Server started successfully on port " + port);
 });
